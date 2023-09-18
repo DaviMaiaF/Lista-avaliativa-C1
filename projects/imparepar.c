f912040d3fc980b0064163ba87a0197714030bc9
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-09-18 09:18:45 -0300

    Criei uma forma de cálculo
    Agora é possível descobrir se o número  é impar ou par

--- projects/imparepar.c
+++ projects/imparepar.c
@@ -2,7 +2,16 @@
 
 int main() {
    
-	printf("Hello world!");
+	int calculo, syn;
+	scanf("%d", &calculo);
+	
+	if(calculo % 2 == 0){
+	 printf("%d eh par\n", calculo);
+	}
+	else{
+	 printf("%d eh impar\n", calculo);
+	}
+	
     return 0;
 }
 
