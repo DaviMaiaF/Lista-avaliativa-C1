553b987703707a11d9a81906f62e8e13e9dbc4ef
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-09-20 20:36:51 -0300

    Criação do projeto de números
    Define se são iguais, múltiplos e qual é o maior

---
+++ projects/valores.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2;
+
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
+
+    if (numero1 == numero2) {
+        printf("Os valores lidos sao iguais\n");
+    } else if (numero1 > numero2) {
+        printf("%d eh maior que %d\n", numero1, numero2);
+    } else {
+    	printf("%d eh maior que %d\n", numero2, numero1);
+	} 
+	
+	if (numero2 != 0) {
+        if (numero1 % numero2 == 0) {
+            printf("%d eh multiplo de %d\n", numero1, numero2);
+        } else {
+            printf("%d nao eh multiplo de %d\n", numero1, numero2);
+        }
+    } 
+    
+    return 0;
+}
