aada91a42de2a6e24d3aca4d795136590f757d69
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-09-20 22:18:29 -0300

    Arrumei os defeitos para os número iguais

--- projects/valores.c
+++ projects/valores.c
@@ -2,11 +2,14 @@
 
 int main() {
     int numero1, numero2;
+    int primeiroNumeroLido = 0;
 
     scanf("%d", &numero1);
+    primeiroNumeroLido = numero1;
+    
     scanf("%d", &numero2);
 
-    if (numero1 == numero2) {
+    if (numero1 == numero2 && primeiroNumeroLido == numero2) {
         printf("Os valores lidos sao iguais\n");
     } else if (numero1 > numero2) {
         printf("%d eh maior que %d\n", numero1, numero2);
