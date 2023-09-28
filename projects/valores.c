cbaa84aabb6f73d0586434108004d2ea21ff260d
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-09-27 21:13:34 -0300

    Mudei a condição para os multiplos

--- projects/valores.c
+++ projects/valores.c
@@ -18,7 +18,7 @@ int main() {
 	} 
 	
 	if (numero1 != numero2) { // Verifica se os números são diferentes
-        if (numero2 != 0 && numero1 % numero2 == 0) {
+        if (numero1 % numero2 == 0 || numero2 % numero1 == 0) {
             printf("%d eh multiplo de %d\n", numero1, numero2);
         } else {
             printf("%d nao eh multiplo de %d\n", numero2, numero1);
