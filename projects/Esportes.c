4f05f5f202283e2109b4b74e39b7b166885b4944
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-09-28 17:26:11 -0300

    Mudanças para os anos especiais

--- projects/Esportes.c
+++ projects/Esportes.c
@@ -11,9 +11,11 @@ int main() {
 		printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
 	} else if(ano == 1942){
 		printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-	} 	if (ano == 1944) {
+	} else if (ano == 1944){
 	    printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-	    } else {
+	} else if (ano == 1946){
+	    	printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
+		} else {
 	    if (ano >= 1930 && (ano - 1930) % 4 == 0) {
 	        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
 	    } else if (ano >= 1896 && (ano - 1896) % 4 == 0) {
